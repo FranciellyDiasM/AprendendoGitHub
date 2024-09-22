5a10997976e5e31f99e5fd426d104bef6abb9181
FranciellyDiasM <frandiasma@gmail.com>
2024-09-22 17:59:22 -0300

    validando entradas invalidas

--- project/src/controller/ImcController.kt
+++ project/src/controller/ImcController.kt
@@ -5,7 +5,12 @@ class ImcController {
         O IMC é o peso dividido pela altura ao quadrado
      */
     fun calculate(weight: Double, height: Double): String {
+        if(height == 0.0 || weight == 0.0) {
+            return "entrada invalida"
+        }
+
         val imc = weight / (height * height)
+
         return "IMC: %.2f".format(imc)
     }
 }
