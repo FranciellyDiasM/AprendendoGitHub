b4cf0c2b03286f14ec7aaeee3cc91fe051c50278
Francielly Dias Macedo <143757045+FranciellyDiasM@users.noreply.github.com>
2024-09-22 18:23:23 -0300

    Revert "implemtancao do conversor de temperatura"

--- project/src/controller/TempConverterController.kt
+++ project/src/controller/TempConverterController.kt
@@ -6,7 +6,7 @@ class TempConverterController {
     O calculo para converter celsius para fahrenheit é: valor * 1.8 + 32
      */
     fun convert(celsius: Double): Double {
-        val fahrenheit = celsius * 1.8 + 32
+        val fahrenheit = celsius
         return fahrenheit
     }
 }
